c281c4de6055dc8b47fa2f9abd62c9592eb2e5c0
loreineb <128940914+loreineb@users.noreply.github.com>
2023-06-13 23:47:23 +0800

    Update TransactionRepository.java
    Added methods to filter by user/transaction type

--- src/main/java/com/gcash/TransactionRepository.java
+++ src/main/java/com/gcash/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.gcash;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class TransactionRepository {
@@ -11,6 +12,7 @@ public class TransactionRepository {
     }
 
     public void addTransaction(String id, Transaction transaction) {
+        transaction.setTimestamp(LocalDateTime.now());
         transactionHistory.putIfAbsent(id, new ArrayList<>());
         //putIfAbsent and put are different
         transactionHistory.get(id).add(transaction);
@@ -28,5 +30,31 @@ public class TransactionRepository {
         }
         return allTransactions;
         //rather than directly return transactionHistory, for encapsulation, just iterate through all entries and add to a separate list
+    }
+
+    public List<Transaction> getTransactionsByType(TransactionRepository transactionRepository, Transaction.TransactionType type) {
+        List<Transaction> filteredTransactions = new ArrayList<>();
+        for (List<Transaction> transactions : transactionHistory.values()) {
+            //iterating through the values of the map, so each value is a list
+            for (Transaction transaction : transactions) {
+                //goes over the values from prior, so yung laman nung list
+                if (transaction.getType() == type) {
+                    //if yung laman ng list is of the same type
+                    filteredTransactions.add(transaction);
+                }
+            }
+        }
+        return filteredTransactions;
+    }
+
+    public List<Transaction> getTransactionsByUser(TransactionRepository transactionRepository, AccountRepository accountRepository, String searchName) {
+        List<Transaction> filteredTransactions = new ArrayList<>();
+
+        for (String id : transactionHistory.keySet()) {
+            if (accountRepository.getAccount(id).getName().equals(searchName)) {
+                filteredTransactions.addAll(transactionHistory.get(id));
+            }
+        }
+        return filteredTransactions;
     }
 }
