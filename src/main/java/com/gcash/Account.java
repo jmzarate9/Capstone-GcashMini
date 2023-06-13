8f80829dd4569bda143af66c14d65131778ddaf6
jmzarate09 <joshuazarate09@gmail.com>
2023-06-13 14:21:11 +0800

    Added Account and AccountRepo class

--- src/main/java/com/gcash/Account.java
+++ src/main/java/com/gcash/Account.java
@@ -1,4 +1,39 @@
 package com.gcash;
 
 public class Account {
+    //Instance Variable
+    private String id;
+    private String name;
+    private String number;
+    private Double balance;
+
+    //Constructor
+    public Account(String id, String name, String number, Double balance) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.balance = balance;
+    }
+
+    //Getter
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    //Setter
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
 }
