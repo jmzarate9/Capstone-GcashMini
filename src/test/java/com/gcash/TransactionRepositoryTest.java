b64ff4f9753c43dbc9425ecbcf4f02b71a511721
kenharvey-delacruz <kenharvey.delacruz@gcash.com>
2023-06-14 20:05:24 +0800

    Update TransactionRepositoryTest.java with comments

--- src/test/java/com/gcash/TransactionRepositoryTest.java
+++ src/test/java/com/gcash/TransactionRepositoryTest.java
@@ -44,18 +44,23 @@ public class TransactionRepositoryTest {
 
     @Test
     void addTransaction() throws NumberAlreadyExistsException, NumberCannotBeEmptyException, NumberMustBeElevenDigitsException, PasscodeCannotBeEmptyException, PasscodeShouldFourDigitsException, NameCannotBeEmptyException {
+//        Setup new accounts to be used for transaction
         accountRepository.userRegistration("09617419366", "Orvyl",0.0, "0000");
         accountRepository.userRegistration("09617419365", "Orvyl2",0.0, "0000");
-        //System.out.println(accountRepository.getAccount(id1));
+//        System.out.println(accountRepository.getAccount(id1));
 
+//        Setup two new transactions
         var transaction1 = new Transaction(1.0, Transaction.TransactionType.DEBIT);
         var transaction2 = new Transaction(5.0, Transaction.TransactionType.CREDIT);
         transactionRepository.addTransaction("09617419366", transaction1);
         transactionRepository.addTransaction("09617419365", transaction2);
 
+//        Call getAllTransactions and store the transactions in List testTransactions
         List<Transaction> testTransactions = transactionRepository.getAllTransactions();
 //        System.out.println(testTransactions.get(0));
 //        System.out.println(testTransactions.get(1));
+
+//        Verify if the transactions stored in the List testTransactions matches the transaction in the variable
         Assertions.assertAll(
                 () -> Assertions.assertEquals(transaction1, testTransactions.get(0)),
                 () -> Assertions.assertEquals(transaction2, testTransactions.get(1))
@@ -64,17 +69,21 @@ public class TransactionRepositoryTest {
 
     @Test
     void getUserTransactions() throws AccountNotFoundException, NumberAlreadyExistsException, NumberCannotBeEmptyException, NumberMustBeElevenDigitsException, PasscodeCannotBeEmptyException, PasscodeShouldFourDigitsException, NameCannotBeEmptyException {
+//        Setup new accounts to be used for transaction
         accountRepository.userRegistration("09617419366", "Orvyl",0.0, "0000");
         accountRepository.userRegistration("09617419365", "Orvyl2",0.0, "0000");
 
+//        Setup three new transactions
         var transaction1 = new Transaction(1.0, Transaction.TransactionType.DEBIT);
         var transaction2 = new Transaction(5.0, Transaction.TransactionType.CREDIT);
         var transaction3 = new Transaction(1.0, Transaction.TransactionType.CREDIT);
 
+//        Add transactions 1 and 3 to 09617419366, add transaction 2 to 09176419365
         transactionRepository.addTransaction("09617419366", transaction1);
         transactionRepository.addTransaction("09617419365", transaction2);
         transactionRepository.addTransaction("09617419366", transaction3);
 
+//        Verify if the transactions added to the numbers matches the number of transactions stored in the user/number
         Assertions.assertAll(
                 () -> Assertions.assertEquals(2, transactionRepository.getUserTransactions("09617419366").size()),
                 () -> Assertions.assertEquals(1, transactionRepository.getUserTransactions("09617419365").size())
@@ -83,13 +92,16 @@ public class TransactionRepositoryTest {
 
     @Test
     void getAllTransactions() throws AccountNotFoundException, NumberAlreadyExistsException, NumberCannotBeEmptyException, NumberMustBeElevenDigitsException, PasscodeCannotBeEmptyException, PasscodeShouldFourDigitsException, NameCannotBeEmptyException {
+//        Setup new accounts to be used for transactions
         accountRepository.userRegistration("09617419366", "Orvyl",0.0, "0000");
         accountRepository.userRegistration("09617419365", "Orvyl2",0.0, "0000");
 
+//        Setup three new transactions
         var transaction1 = new Transaction(1.0, Transaction.TransactionType.DEBIT);
         var transaction2 = new Transaction(5.0, Transaction.TransactionType.CREDIT);
         var transaction3 = new Transaction(1.0, Transaction.TransactionType.CREDIT);
 
+//        Add transactions 1 and 3 to 09617419366, add transaction 2 to 09176419365
         transactionRepository.addTransaction("09617419366", transaction1);
         transactionRepository.addTransaction("09617419365", transaction2);
         transactionRepository.addTransaction("09617419366", transaction3);
@@ -97,23 +109,30 @@ public class TransactionRepositoryTest {
 //        System.out.println(transactionRepository.getUserTransactions(id1));
 //        System.out.println(transactionRepository.getUserTransactions(id2));
 
+//        Call getAllTransactions and store the transactions in List testTransactions
         List<Transaction> testTransactions = transactionRepository.getAllTransactions();
+
+//        Verify if the size of List testTransactions matches the expected value (3)
         Assertions.assertEquals(3, testTransactions.size());
     }
 
     @Test
     void getTransactionsByType() throws AccountNotFoundException, NumberAlreadyExistsException, NumberCannotBeEmptyException, NumberMustBeElevenDigitsException, PasscodeCannotBeEmptyException, PasscodeShouldFourDigitsException, NameCannotBeEmptyException {
+//        Setup new accounts to be used for transactions
         accountRepository.userRegistration("09617419366", "Orvyl",0.0, "0000");
         accountRepository.userRegistration("09617419365", "Orvyl2",0.0, "0000");
 
+//        Setup three new transactions (2 TRANSFER, 1 CREDIT)
         var transaction1 = new Transaction(1.0, Transaction.TransactionType.TRANSFER);
         var transaction2 = new Transaction(5.0, Transaction.TransactionType.TRANSFER);
         var transaction3 = new Transaction(1.0, Transaction.TransactionType.CREDIT);
 
+//        Add transactions 1 and 3 to 09617419366, add transaction 2 to 09176419365
         transactionRepository.addTransaction("09617419366", transaction1);
         transactionRepository.addTransaction("09617419365", transaction2);
         transactionRepository.addTransaction("09617419366", transaction3);
 
+//        Verify the number of transactions by type by checking if it matches the expected value of 2 TRANSFER, 1 CREDIT transactions
         Assertions.assertAll(
                 () -> Assertions.assertEquals(1, transactionRepository.getTransactionsByType(transactionRepository, Transaction.TransactionType.CREDIT).size()),
                 () -> Assertions.assertEquals(2, transactionRepository.getTransactionsByType(transactionRepository, Transaction.TransactionType.TRANSFER).size())
@@ -123,15 +142,25 @@ public class TransactionRepositoryTest {
 
     @Test
     void noUserTransactionsToGet() throws AccountNotFoundException, NumberAlreadyExistsException, NumberCannotBeEmptyException, NumberMustBeElevenDigitsException, PasscodeCannotBeEmptyException, PasscodeShouldFourDigitsException, NameCannotBeEmptyException{
+//        Setup new account to be used for transaction
         accountRepository.userRegistration("09617419366", "Orvyl",0.0, "0000");
+
+//        Check if specific number has no transactions by calling the isEmpty method, and storing it in boolean checkContents
         boolean checkContents = transactionRepository.getUserTransactions("09617419366").isEmpty();
+
+//        Verify that boolean checkContents is empty if true
         Assertions.assertTrue(checkContents);
     }
 
     @Test
     void noTransactionsAtAll() throws AccountNotFoundException, NumberAlreadyExistsException, NumberCannotBeEmptyException, NumberMustBeElevenDigitsException, PasscodeCannotBeEmptyException, PasscodeShouldFourDigitsException, NameCannotBeEmptyException{
+//        Call getAllTransactions and store the transactions in List TestTransactions
         List<Transaction> testTransactions = transactionRepository.getAllTransactions();
+
+//        Check if the list testTransactions is empty by calling the isEmpty method, and storing it in boolean checkContents
         boolean checkContents = testTransactions.isEmpty();
+
+//        Verify that the boolean checkContents is empty if true
         Assertions.assertTrue(checkContents);
     }
 
